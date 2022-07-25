4110c72a7825d1c6ab9f0e68b37d3cfdf4d9e639
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-07-25 17:18:19 -0300

    Consegui

---
+++ aula06/TiposPrimitivos2/src/tiposprimitivos2/TiposPrimitivos2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivos2;
+
+/**
+ *
+ * @author lukas
+ */
+public class TiposPrimitivos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome = "Lucas";
+        float nota = 8.5f;
+        System.out.format("A nota de %s é %.2f", nome,  nota);
+    }
+    
+}
