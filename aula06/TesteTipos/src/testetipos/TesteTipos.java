f0b869527cf69031b0fbaa527173ee98335c6824
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-07-25 21:54:12 -0300

    COMMITEI

--- aula06/TesteTipos/src/testetipos/TesteTipos.java
+++ aula06/TesteTipos/src/testetipos/TesteTipos.java
@@ -15,6 +15,13 @@ public class TesteTipos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);*/
+        
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
     }
     
 }
