d9f8f34085862ddc00cb4df7e3ad38762b289581
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-07-27 10:13:39 -0300

    loucura

--- aula07/Operadoresaritmeticos/src/operadoresaritmeticos/Operadoresaritmeticos.java
+++ aula07/Operadoresaritmeticos/src/operadoresaritmeticos/Operadoresaritmeticos.java
@@ -8,13 +8,17 @@ package operadoresaritmeticos;
  *
  * @author lukas
  */
-public class OperadoresAritmeticos {
+public class Operadoresaritmeticos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual à " + m);
     }
     
 }
