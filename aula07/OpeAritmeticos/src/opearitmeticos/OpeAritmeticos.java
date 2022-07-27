8ef0edd7a640dcb1e33c1e3080c16de3ad7635c6
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-07-27 13:16:09 -0300

    Atualizado

--- aula07/OpeAritmeticos/src/opearitmeticos/OpeAritmeticos.java
+++ aula07/OpeAritmeticos/src/opearitmeticos/OpeAritmeticos.java
@@ -15,10 +15,19 @@ public class OpeAritmeticos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2)/2;
-        System.out.println("A média é igual à " + m);
+        System.out.println("A média é igual à " + m);*/
+        
+        /*int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero);*/
+        
+        int x = 4;
+        x *= 2;
+        System.out.println(x);
     }
     
 }
