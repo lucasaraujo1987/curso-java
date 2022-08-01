1541f56a379371a26b9598c0dbb30ef7e52d4a22
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-08-01 20:20:15 -0300

    Criei aula 13

---
+++ aula13/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author lukas
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*for (int cc=0; cc<=3; cc++){
+            System.out.println(cc);
+        }*/
+        
+        for (int i=0; i<4; i++){
+            for (int j=0; j<3; j++){
+                System.out.print(i);
+                System.out.println(j);
+            }
+        }
+    }
+    
+}
