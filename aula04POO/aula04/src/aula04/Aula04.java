655acdf6c617553f4ae81e2321e9b8cc61268d24
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-08-05 18:10:05 -0300

    criei aula 04 POO

---
+++ aula04POO/aula04/src/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC","Amarela", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("KKK","Laranja", 0.7f);
+        c2.status();
+    }
+    
+}
