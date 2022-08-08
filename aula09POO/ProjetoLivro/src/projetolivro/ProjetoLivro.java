2618f0ac83cb18734ac001bd4883991adec7f7eb
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-08-08 17:09:11 -0300

    criei aula09POO

---
+++ aula09POO/ProjetoLivro/src/projetolivro/ProjetoLivro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetolivro;
+public class ProjetoLivro {
+    public static void main(String[] args) {
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+        
+        p[0] = new Pessoa("Pedro", 22, "M");
+        p[1] = new Pessoa("Maria", 25, "F");
+        
+        l[0] = new Livro("Aprendendo JAVA", "José da Silva", 300, p[0]);
+        l[1] = new Livro("POO para iniciantes", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro("Java Avançado", "Maria Candido", 800, p[0]);
+        
+        l[0].abrir();
+        l[0].folhear(200);
+        l[0].avancarPag();
+        System.out.println(l[0].detalhes());
+        
+        System.out.println(l[1].detalhes());
+        
+        System.out.println(l[2].detalhes());
+    }
+    
+}
