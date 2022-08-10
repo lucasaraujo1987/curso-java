c24d6427bfd598a880a0ffbd933ecce85b366cbd
Lucas Sebastião de Araújo <101892720+lucasaraujo1987@users.noreply.github.com>
2022-08-09 23:05:45 -0300

    criei aula13POO

---
+++ aula13POO/Aula13/src/aula13/Aula13.java
@@ -0,0 +1,11 @@
+package aula13;
+public class Aula13 {
+    public static void main(String[] args) {
+        //Programa Principal
+        Cachorro c = new Cachorro();
+        
+        c.reagir(true);
+        c.reagir(false);
+    }
+    
+}
